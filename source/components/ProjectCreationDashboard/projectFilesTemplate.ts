dbfe78ad90abd281670833d2865f09d9ed107a04
sawinjer <polisukandrij68@gmail.com>
2025-07-29 00:19:55 +0300

    add cache to initial template project

--- source/components/ProjectCreationDashboard/projectFilesTemplate.ts
+++ source/components/ProjectCreationDashboard/projectFilesTemplate.ts
@@ -20,31 +20,53 @@ services:
     volumes:
       - ./data/database:/var/lib/postgresql/data
     environment:
-      POSTGRES_USER: 'directus'
-      POSTGRES_PASSWORD: 'directus'
-      POSTGRES_DB: 'directus'
+      POSTGRES_USER: "directus"
+      POSTGRES_PASSWORD: "directus"
+      POSTGRES_DB: "directus"
+    healthcheck:
+      test: ["CMD", "pg_isready", "--host=localhost", "--username=directus"]
+      interval: 10s
+      timeout: 5s
+      retries: 5
+      start_interval: 5s
+      start_period: 30s
+  cache:
+    image: redis:6
+    healthcheck:
+      test: ["CMD-SHELL", "[ $$(redis-cli ping) = 'PONG' ]"]
+      interval: 10s
+      timeout: 5s
+      retries: 5
+      start_interval: 5s
+      start_period: 30s
   directus:
     image: directus/directus:latest
     volumes:
+      - ./uploads:/directus/uploads
       - ./${payload.devTarget}:/directus/extensions
     ports:
       - ${payload.port}:8055
     depends_on:
       database:
-        condition: service_started
+        condition: service_healthy
+      cache:
+        condition: service_healthy
     environment:
       KEY: '${key}'
       SECRET: '${secret}'
       ADMIN_EMAIL: '${payload.login}'
       ADMIN_PASSWORD: '${payload.password}'
-      DB_CLIENT: 'pg'
-      DB_PORT: '5432'
-      DB_HOST: 'database'
-      DB_DATABASE: 'directus'
-      DB_USER: 'directus'
-      DB_PASSWORD: 'directus'
+      DB_CLIENT: "pg"
+      DB_HOST: "database"
+      DB_PORT: "5432"
+      DB_DATABASE: "directus"
+      DB_USER: "directus"
+      DB_PASSWORD: "directus"
+      CACHE_ENABLED: "true"
+      CACHE_AUTO_PURGE: "true"
+      CACHE_STORE: "redis"
+      REDIS: "redis://cache:6379"
       EXTENSIONS_AUTO_RELOAD: true
-      MAX_PAYLOAD_SIZE: '100mb'
 `.trim();
 };
 
